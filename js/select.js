ce43d16b68dda739bc6eec8724c93363d01b1b20
Md-TarikulIslam <nayon1820@gmail.com>
2022-08-22 04:26:37 +0600

    first calculation added

--- js/select.js
+++ js/select.js
@@ -24,6 +24,7 @@ function display(playerCount) {
     }
 }
 
+
 function playerList(player) {
 
 
@@ -37,4 +38,49 @@ function playerList(player) {
 
     document.getElementById('total-player').innerText = listArray.length;
     display(listArray)
+
+    document.getElementById('btn-perplayer-calculate').addEventListener('click', function () {
+        const budgetPerplayerInput = document.getElementById('budget-perplayer');
+        const budgetPerplayerString = budgetPerplayerInput.value;
+        const budgetPerplayer = parseFloat(budgetPerplayerString);
+
+        //Get value of budget per player
+
+
+        const playerExpensesMoney = document.getElementById('player-expenses');
+        const playerExpensesMoneyString = playerExpensesMoney.innerText;
+        const playerExpenses = parseFloat(playerExpensesMoneyString);
+        // Calculation
+        const playerExpensesTotal = (budgetPerplayer * listArray.length);
+        playerExpensesMoney.innerText = playerExpensesTotal;
+
+
+
+    });
+
+    document.getElementById('btn-total-budget').addEventListener('click', function () {
+        const managerField = document.getElementById('manager-field');
+        const managerFieldString = managerField.value;
+        const manager = parseFloat(managerFieldString);
+
+
+        const coachField = document.getElementById('coach-field');
+        const coachFieldString = coachField.value;
+        const coach = parseFloat(coachFieldString);
+
+
+
+
+        const totalMoney = document.getElementById('total');
+        const totalMoneyString = totalMoney.innerText;
+        const money = parseFloat(totalMoneyString);
+
+
+
+        const sum = manager + coach + playerExpensesTotal;
+        totalMoney.innerText = sum;
+
+    })
+
+
 }
